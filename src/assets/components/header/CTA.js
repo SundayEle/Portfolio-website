762e1fec36f1644f7a068912cc563cc320f0cc0b
SundayEle <ikpabisunday91@gmail.com>
2024-10-07 12:43:25 +0100

    changes made to cv

--- src/assets/components/header/CTA.js
+++ src/assets/components/header/CTA.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import CV from "../../../assets/sundaycv.pdf"
-import { Slide } from 'react-awesome-reveal'
+import React from "react";
+import CV from "../../../assets/2024_IKPABI_RESUME.pdf";
+import { Slide } from "react-awesome-reveal";
 
 function Cta() {
   return (
-    <div className='cta'>
-      <Slide direction='left'>
-        <a href={CV} download className='btn'>Download CV</a>
+    <div className="cta">
+      <Slide direction="left">
+        <a href={CV} download className="btn">
+          Download CV
+        </a>
       </Slide>
-      <Slide direction='right'>
-      <a href="#contact" className='btn btn-primary'>Talk to me</a>
+      <Slide direction="right">
+        <a href="#contact" className="btn btn-primary">
+          Talk to me
+        </a>
       </Slide>
-      
     </div>
-     
-  )
+  );
 }
 
-export default Cta
+export default Cta;
